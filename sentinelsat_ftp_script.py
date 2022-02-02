b1d538aac43bcd61ae17acf2f5acee5d196d2f1f
owinton <43796798+owinton@users.noreply.github.com>
2022-02-02 11:45:18 +0100

    Updated FTP script

--- sentinelsat_ftp_script.py
+++ sentinelsat_ftp_script.py
@@ -7,21 +7,27 @@ Created by kaha on 25 Jun 2018
 Last edited by oew on 10 Mar 2021
 """
 def sentinelsat_ftp_upload(output_plots_to_upload, output_plots_uploaded):
-	ftp = ftplib.FTP('ftpserver.dmi.dk')
+    ftp = ftplib.FTP('ftpserver.dmi.dk')
+    print(ftp.getwelcome());
+
+    # Credentials are stored in txt file not shared in the public repo.
     with open('../username_password.txt', 'r') as file:
         data = file.readlines()
         data = [d.replace('\n', '') for d in data]
     USERNAME = data[2]
     PASSWORD = data[3]
-	ftp.login(USERNAME, PASSWORD)
-	ftp.cwd('upload')
-	upload_list = [i for i in output_plots_to_upload if i.endswith('.png')]
-	for upload_filename in upload_list:
-		file = open(os.path.join(output_plots_to_upload, upload_filename, 'rb')
-	    #ftp.storbinary(upload_filename, file)
-		print('Uploaded {}'.format(filename))
 
-		# Move to uploaded files folder
-		shutil.move(os.path.join(output_plots_to_upload, upload_filename), os.path.join(output_plots_uploaded, upload_filename))
+    ftpResponse = ftp.login(USERNAME, PASSWORD)
+    print(ftpResponse)
+    ftpResponse = ftp.cwd('upload')
+    print(ftpResponse)
 
-	ftp.quit()
+    upload_list = [i for i in os.listdir(output_plots_to_upload) if i.endswith('.png')]
+    for upload_filename in upload_list:
+        file = open(os.path.join(output_plots_to_upload, upload_filename), 'rb')
+        ftpCommand = "STOR {}".format(upload_filename);
+        ftpResponse = ftp.storbinary(ftpCommand, file)
+        print(ftpResponse)
+        print('Uploaded {}'.format(upload_filename))
+        shutil.move(os.path.join(output_plots_to_upload, upload_filename), os.path.join(output_plots_uploaded, upload_filename))
+    ftp.quit()
